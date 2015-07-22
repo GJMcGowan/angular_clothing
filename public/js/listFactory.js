9e706b28f89d06396a0e8d84c88efcfb8db84939
GJMcGowan <gemcgow@gmail.com>
2015-07-22 13:43:30 +0100

    Reworked voucher system and added more tests

--- public/js/listFactory.js
+++ public/js/listFactory.js
@@ -1,26 +1,78 @@
 clothesShop.factory('List', [function() {
   var service = {};
   var cartList = [];
-  var cartPrice = 0
+  var vouchers = [];
 
   service.cartList = cartList;
-  service.cartPrice = cartPrice;
+
+  service.getCartPrice = function(){
+    var result = 0
+    for (var i = cartList.length - 1; i >= 0; i--) {
+      result += cartList[i].price;
+    };
+    for (var i = vouchers.length - 1; i >= 0; i--) {
+      result -= vouchers;
+    };
+    // Currently the way to deal with JS number oddness
+    result = parseFloat(result.toPrecision(12));
+    return result;
+  };
 
   service.addProduct = function(item) {
     if(item.quantity >= 1) {
       cartList.push(item);
       item.quantity -= 1;
-      cartPrice += item.price;
     } else {
       // Flash.create('danger', 'That item is out of stock', 'stockError');
     };
   };
 
   service.removeProduct = function(item) {
-    cartPrice -= item.price;
     var index = cartList.indexOf(item)
     item.quantity += 1;
     cartList.splice(index, 1);
+  };
+
+  service.applyVoucher = function(voucher) {
+    var used = false;
+    for(i = vouchers.length - 1; i >= 0; i--){
+      if(vouchers[i] === voucher) {
+        used = true;
+      };
+    };
+    if(voucher === 5) {
+      vouchers.push(voucher)
+    } else if (voucher === 10) {
+      service.applyVoucher10();
+    } else if (voucher === 15) {
+      service.applyVoucher15();
+    } else {
+      // Flash.create('danger', 'Invalid voucher', 'error10');
+    };
+  };
+
+  service.applyVoucher10 = function() {
+    if(service.getCartPrice() >= 50) {
+      vouchers.push(10);
+    } else {
+      // var show = 'You need at least £50 of items to use that voucher'
+      // Flash.create('danger', show, 'error10');
+    };
+  };
+
+  service.applyVoucher15 = function() {
+    var shoes = false;
+    for (var i = cartList.length - 1; i >= 0; i--) {
+      if(cartList[i].category.indexOf("Footwear") >= 0) {
+        shoes = true;
+      };
+    };
+    if(service.getCartPrice() >= 75 && shoes) {
+      vouchers.push(15);
+    } else {
+      // var show = 'You need at least £75 of items and an item of footwear to use that voucher'
+      // Flash.create('danger', show, 'error15');
+    };
   };
 
   return service;
--- test/listFactory.spec.js
+++ test/listFactory.spec.js
@@ -9,7 +9,10 @@ describe('factory: List', function() {
 
   beforeEach(inject(function(List) {
     list = List
-    sample = {name: "Suede Shoes, Blue", price: 42.00, quantity: 4, category: "Womens Footwear"};
+    sample = {name: "Suede Shoes, Blue", price: 42.00,
+              quantity: 4, category: "Womens Footwear"};
+    not_shoes = {name: "Fine Stripe Short Sleeve Shirt, Grey", price: 49.99,
+                 quantity: 9, category: "Mens Casual"}
   }));
 
   describe('cart', function() {
@@ -19,7 +22,7 @@ describe('factory: List', function() {
     });
 
     it('total price begins at zero', function() {
-      expect(list.cartPrice).toEqual(0);
+      expect(list.getCartPrice()).toEqual(0);
     });
 
     it('can add an item to the cart', function() {
@@ -39,39 +42,51 @@ describe('factory: List', function() {
       expect(list.cartList).toEqual([]);
     });
 
-    // it('knows the total price of items in the cart', function() {
-    //   list.addProduct(sample);
-    //   expect(list.cartPrice).toEqual(42);
-    // });
+    it('knows the total price of items in the cart', function() {
+      list.addProduct(sample);
+      expect(list.getCartPrice()).toEqual(42);
+    });
 
-    // it('adding an item to the cart lowers the quantity', function() {
-    //   expect(sample.quantity).toEqual(4);
-    //   list.addProduct(sample);
-    //   expect(sample.quantity).toEqual(3);
-    // });
+    it('adding an item to the cart lowers the quantity', function() {
+      expect(sample.quantity).toEqual(4);
+      list.addProduct(sample);
+      expect(sample.quantity).toEqual(3);
+    });
 
-    // it('cannot add out of stock items to the cart', function() {
-    //   for (var i = 10 - 1; i >= 0; i--) {
-    //     list.addProduct(sample);
-    //   };
-    //   expect(list.cartList.length).toEqual(4);
-    // });
+    it('cannot add out of stock items to the cart', function() {
+      for (var i = 10 - 1; i >= 0; i--) {
+        list.addProduct(sample);
+      };
+      expect(list.cartList.length).toEqual(4);
+    });
   });
 
-  // describe('vouchers', function() {
-  //   it('can apply £5 voucher at any time', function() {
-  //     list.addProduct(sample);
-  //     list.applyVoucher(5);
-  //     expect(list.cartPrice).toEqual(37);      
-  //   });
+  describe('vouchers', function() {
+    it('can apply £5 voucher at any time', function() {
+      list.addProduct(sample);
+      list.applyVoucher(5);
+      expect(list.getCartPrice()).toEqual(37);      
+    });
 
-  //   it('cannot apply a £10 voucher unless the total price is over £50', function() {
-  //     list.addProduct(sample);
-  //     list.applyVoucher(10);
-  //     expect(list.cartPrice).toEqual(42);
-  //     list.addProduct(sample);
-  //     list.applyVoucher(10);
-  //     expect(list.cartPrice).toEqual(74);
-  //   });
-  // });
+    it('cannot apply a £10 voucher unless the total price is over £50', function() {
+      list.addProduct(sample);
+      list.applyVoucher(10);
+      expect(list.getCartPrice()).toEqual(42);
+      list.addProduct(sample);
+      list.applyVoucher(10);
+      expect(list.getCartPrice()).toEqual(74);
+    });
+
+    it('cannot apply £15 voucher unless total price >= £75 and shoes were bought', function() {
+      list.addProduct(not_shoes);
+      list.applyVoucher(15);
+      expect(list.getCartPrice()).toEqual(49.99);
+      list.addProduct(not_shoes);
+      list.applyVoucher(15);
+      expect(list.getCartPrice()).toEqual(99.98);
+      list.addProduct(sample);
+      list.applyVoucher(15);
+      expect(list.getCartPrice()).toEqual(126.98);
+    });
+  });
 });
